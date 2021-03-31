8ab9caea6de176a3ab52dba7f3eb3ec81be36577
Benny <benny.van.meerbeeck@student.howest.be>
2021-04-01 01:45:04 +0200

    Add select option
    with ActivateOption method global var and handler

--- js/scriptGui.js
+++ js/scriptGui.js
@@ -1,7 +1,24 @@
 "use strict"
 
 window.addEventListener("load",init)
+var select;
 
 function init(){
-    
+    select = document.getElementById("idSelectOption");
+    select.addEventListener("change",ActivateOption);
+   
+}
+
+function ActivateOption(){
+    let priem = document.getElementById("idPriemWrapper");
+    let convert = document.getElementById("idConvert");
+
+    priem.classList.remove("hide");
+    convert.classList.remove("hide");
+
+    if(select.value == "priemgetal"){        
+        convert.classList.add("hide");
+    }else{        
+        priem.classList.add("hide");
+    }
 }
