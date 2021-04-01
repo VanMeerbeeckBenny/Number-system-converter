7f22fa8f2036ddbc251b19d576504eb0a6bf98c3
Benny <benny.van.meerbeeck@student.howest.be>
2021-04-01 16:33:52 +0200

    Add global variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+"use strict"
 
+window.addEventListener("load",init)
+var inputpriem;
+var inputconvert;
+var base;
+
+var btnCheckPriem;
+var btnConvert;
+
+function init(){
+   inputpriem = document.getElementById("idPriem");
+   inputconvert = document.getElementById("idConvert");
+   base = document.getElementById("idBaseNr");
+
+   btnCheckPriem = document.getElementById("checkPriemBtn");;
+   btnConvert = document.getElementById("idConvertBtn");;   
+}
 
 
